ad3dd1b064451b856a4190802f3c942053fa582f
Molly Venegas <mollyvenegasvasquez@gmail.com>
2026-03-17 16:28:51 -0300

    terminando tarjeta de entrenadores

--- src/sections/Entrenadores.jsx
+++ src/sections/Entrenadores.jsx
@@ -1,7 +1,18 @@
+import entrenadores from "../assets/entrenadores.jpeg";
+
 function Entrenadores() {
   return (
     <section id="entrenadores">
-      <h2>entrenadores</h2>
+      <h2>Entrenadores</h2>
+
+      <div className="cards">
+        <div className="card">
+          <img src={entrenadores} />
+          <h3>Melody Venegas - Andres Norambuena</h3>
+          <p>Entrenadores deportivos</p>
+        </div>
+
+      </div>
     </section>
   );
 }
