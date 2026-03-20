8572c729c53cdb7ea0eb4ad0d6709736308f2931
Alfredo Inostroza Leon <69522650+ArticosDev@users.noreply.github.com>
2026-03-21 11:36:02 +1300

    Fix merged flame rail styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,23 +11,49 @@ import Galeria from "../sections/Galeria";
 import Indumentaria from "../sections/Indumentaria";
 import Unete from "../sections/Unete";
 import Contacto from "../sections/Contacto";
+import fuego from "../assets/fuego.png";
+
+const flameSlots = ["flame-slot-top", "flame-slot-middle", "flame-slot-bottom"];
+
+function FlameRail({ side }) {
+  return (
+    <div className={`flame-rail flame-rail-${side}`}>
+      {flameSlots.map((slot) => (
+        <img
+          key={`${side}-${slot}`}
+          src={fuego}
+          alt=""
+          aria-hidden="true"
+          className={`flame ${slot}`}
+        />
+      ))}
+    </div>
+  );
+}
 
 function Home() {
   return (
-    <div>
-      <Navbar />
-      <Inicio />
-      <QuienesSomos />
-      <Metodologia />
-      <Entrenadores />
-      <Valores />
-      <MisionVision />
-      <Competencia />
-      <Galeria />
-      <Indumentaria />
-      <Unete />
-      <Contacto />
-      <Footer />
+    <div className="site-shell">
+      <div className="site-flames" aria-hidden="true">
+        <FlameRail side="left" />
+        <FlameRail side="right" />
+      </div>
+
+      <div className="site-content">
+        <Navbar />
+        <Inicio />
+        <QuienesSomos />
+        <Metodologia />
+        <Entrenadores />
+        <Valores />
+        <MisionVision />
+        <Competencia />
+        <Galeria />
+        <Indumentaria />
+        <Unete />
+        <Contacto />
+        <Footer />
+      </div>
     </div>
   );
 }
